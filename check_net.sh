7a7fbcec27ef1db8fd00a3dcc222cf25174c467d
confiq <github.com@confiq.org>
2017-10-20 14:16:45 +0300

    printing how much time internet was down

--- check_net.sh
+++ check_net.sh
@@ -7,6 +7,12 @@ readonly ip_addresses=("8.8.8.8" "8.8.4.4" "4.2.2.2")
 readonly dns_addresses=("google.com" "facebook.com") # can add a check dig +short fqdn
 readonly sleep_cycle=5 # sleep cycle in seconds
 internet_is_down='false'
+
+# functions
+timestamp() {
+	date +"%s"
+}
+# funtime
 while true;
 do
 	date="[$(date +'%b %d, %r')] ";
@@ -26,6 +32,7 @@ do
 	then
 		if [ $internet_is_down = 'false' ]
 		then 
+			total_down=$(timestamp)
 			internet_is_down='true'
 			echo "$date INTERNET_DOWN" >> ~/status.txt;
 		#else
@@ -35,7 +42,10 @@ do
 	else
 		if [ $internet_is_down = 'true' ]
 		then
-			echo "$date INTERNET_UP" >> ~/status.txt
+			total_down=$(expr $(timestamp) - $total_down)
+			echo -n $total_down
+			nice_total_down=$(date +%T -d "1/1 + $total_down sec")
+			echo "$date INTERNET_UP $nice_total_down" >> ~/status.txt
 			internet_is_down='false'
 		fi
 		echo -n "."
